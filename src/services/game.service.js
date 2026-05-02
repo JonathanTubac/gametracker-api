df4b865a635be6a25de2062bf21dfc6ccf246d5f
JonathanTubac <javiertubac1290.e@gmail.com>
2026-05-02 13:44:00 -0600

    feat(games): Added service

---
+++ src/services/game.service.js
@@ -0,0 +1,39 @@
+import * as gamesRepo from '../repositories/game.repository.js'
+import { BadRequest, InternalServer, NotFoundError } from '../utils/errors.js'
+
+export const getGames = async () => {
+    return await gamesRepo.findAll();
+};
+
+export const getGame = async (id) => {
+    const game = await gamesRepo.findById(id);
+    if(!game) throw new NotFoundError('That game doesnt exist!');
+
+    return game;
+}
+
+export const createGame = async ({title, dev, genre, platform, release, image, notes}) => {
+    if (!title || !dev || !genre || !platform || !release || !image || !notes) throw new BadRequest('Fields missing!');
+
+    const created = await gamesRepo.create({title, dev, genre, platform, release, image, notes});
+    if(!created) throw new InternalServer('Cant create game!');
+
+    return created;
+}
+
+export const updateGame = async (id, {title, dev, genre, platform, release, status, hours, image, notes}) => {
+    const game = await gamesRepo.findById(id);
+    if(!game) throw new NotFoundError('That game doesnt exist!');
+
+    const updated = await gamesRepo.update(id, {title, dev, genre, platform, release, status, hours, image, notes});
+    if(!updated) throw new InternalServer('Cant update game!')
+
+    return updated;
+}
+
+export const deleteGame = async (id) => {
+    const game = await gamesRepo.findById(id);
+    if(!game) throw new NotFoundError('That game doesnt exist!');
+
+    await gamesRepo.del(id);
+}
---
+++ src/utils/errors.js
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+    constructor(message, statusCode, code) {
+        super(message);
+        this.statusCode = statusCode;
+        this.code = code;
+        this.isOperational = true
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor(msg = 'Not found') {
+        super(msg, 404, 'NOT_FOUND');
+    }
+}
+
+export class BadRequest extends AppError {
+    constructor (msg = 'Fields missing') {
+        super(msg, 400, 'BAD_REQUEST');
+    }
+}
+
+export class InternalServer extends AppError {
+    constructor (msg = 'Internal server error') {
+        super(msg, 500, 'INTERNAL_SV_ERROR');
+    }
+}
