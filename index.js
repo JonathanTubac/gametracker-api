387694ec0dddb0b41180df88869b5b6a9584f3ce
JonathanTubac <javiertubac1290.e@gmail.com>
2026-05-06 16:14:24 -0600

    fix(vercel): vercel config

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { connect } from './src/config/db.js';
 import gameRoutes from './src/routes/game.routes.js'
 import ratingRoutes from './src/routes/rating.routes.js'
 import cors from 'cors'
@@ -8,10 +7,7 @@ import errorMiddleware from './src/middlewares/error.middleware.js';
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
@@ -27,11 +23,6 @@ app.use('/api/v1/games', gameRoutes);
 app.use('/api/v1/ratings', ratingRoutes);
 app.use(errorMiddleware);
 
-await connect();
-
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+// await connect();
 
 export default app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,16 @@
 import { Pool } from "pg";
-import 'dotenv/config'
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-    ssl: process.env.NODE_ENV === 'production'
-        ? {rejectUnauthorized: false}
-        : false
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 export const connect = async () => {
-    try {
-        await pool.query(`
-           SELECT 1 
-        `);
-        console.log("✅Connected with postgresql");
-    } catch (err) {
-        console.log("❌ Error connecting with postgres")
-        process.exit(1)
-    }
+  try {
+    await pool.query('SELECT 1');
+    console.log("✅ Connected with postgresql");
+  } catch (err) {
+    console.error("❌ Error connecting with postgres:", err.message);
+    throw err;
+  }
 }
